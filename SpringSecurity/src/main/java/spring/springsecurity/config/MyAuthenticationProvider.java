3fc871ee1861ffbe002e70391da298c03b779920
VinhLB1997 <43948809+VinhLB1997@users.noreply.github.com>
2022-12-11 20:27:54 +0700

    SpringSecurity #2 Custom using AuthenticationProvider (custom logic authen)

--- SpringSecurity/src/main/java/spring/springsecurity/config/MyAuthenticationProvider.java
+++ SpringSecurity/src/main/java/spring/springsecurity/config/MyAuthenticationProvider.java
@@ -1,32 +1,52 @@
 package spring.springsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import spring.springsecurity.entity.CustomerEntity;
+import spring.springsecurity.repository.CustomerRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-//@Component
+@Component
 public class MyAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
-        if("vinhtn".equals(username) && "123456".equals(password)){
-            return UsernamePasswordAuthenticationToken.authenticated(username, password, Arrays.asList());
+        String email = authentication.getName();
+        String passwordAuth = authentication.getCredentials().toString();
+        Optional<CustomerEntity> customerFound = customerRepository.findByEmail(email);
+        if (customerFound.isPresent()) {
+            if (passwordEncoder.matches(passwordAuth, customerFound.get().getPassword())) {
+                List<GrantedAuthority> roles = new ArrayList<>();
+                roles.add(new SimpleGrantedAuthority(customerFound.get().getRole()));
+                return UsernamePasswordAuthenticationToken.authenticated(email, passwordAuth, roles);
+            } else {
+                throw new BadCredentialsException("Invalid password!");
+            }
         } else {
-            throw new BadCredentialsException("Error Author");
+            throw new BadCredentialsException("No user registered with this details!");
         }
     }
 
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+        @Override
+        public boolean supports (Class < ? > authentication){
+            return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+        }
     }
-}
--- SpringSecurity/src/main/java/spring/springsecurity/service/CustomerService.java
+++ SpringSecurity/src/main/java/spring/springsecurity/service/CustomerService.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+//@Service
 public class CustomerService implements UserDetailsService {
 
     @Autowired
